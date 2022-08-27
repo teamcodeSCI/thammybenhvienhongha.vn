185cb8bb2cf2bdf9499d5269529741712aab28d1
doanduc2000 <doanminhducvip2000@gmail.com>
2022-08-27 14:05:07 +0700

    update

---
+++ Home/doctor_dhh_2_0_0/js/doctor_dhh_2_0_0.js
@@ -0,0 +1,53 @@
+const doctorInfo = [{
+    name: 'Dr Robert Nguyễn',
+    img: 'Home/doctor_dhh_2_0_0/images/doctor-detail1.jpg',
+    text: [
+        'Chuyên gia hàng đầu trong lĩnh vực PTTM1',
+        'Thành viên Hiệp hội PTTM Hàn Quốc',
+        '15 năm kinh nghiệm PTTM tạo hình khuôn mặt và vóc dáng',
+        'Thực hiện thành công > 1000 ca PTTM mỗi năm'
+    ]
+}, {
+    name: 'Dr Robert Nguyễn1',
+    img: 'Home/doctor_dhh_2_0_0/images/doctor-detail1.jpg',
+    text: [
+        'Chuyên gia hàng đầu trong lĩnh vực PTTM2',
+        'Thành viên Hiệp hội PTTM Hàn Quốc',
+        '15 năm kinh nghiệm PTTM tạo hình khuôn mặt và vóc dáng',
+        'Thực hiện thành công > 1000 ca PTTM mỗi năm'
+    ]
+}, {
+    name: 'Dr Robert Nguyễn2',
+    img: 'Home/doctor_dhh_2_0_0/images/doctor-detail1.jpg',
+    text: [
+        'Chuyên gia hàng đầu trong lĩnh vực PTTM3',
+        'Thành viên Hiệp hội PTTM Hàn Quốc',
+        '15 năm kinh nghiệm PTTM tạo hình khuôn mặt và vóc dáng',
+        'Thực hiện thành công > 1000 ca PTTM mỗi năm'
+    ]
+}]
+const infoTemp = (props) => {
+    let text = ''
+    for (let item of props.text) {
+        text += ` <li>– ${item}</li>`
+    }
+    return ` <div class="doctor_dhh_2_0_0__detail doctor_dhh_2_0_0--active">
+    <div class="doctor_dhh_2_0_0__img">
+        <img width="366" height="356" src="${props.img}" alt="">
+    </div>
+    <div class="doctor_dhh_2_0_0__text">
+        <span>${props.name}</span>
+        <ul>
+           ${text}
+        </ul>
+    </div>
+</div>`
+}
+document.getElementById('doctor_dhh_2_0_0__content').innerHTML = infoTemp(doctorInfo[0])
+
+const tabDoctors = document.getElementsByClassName('doctor_dhh_2_0_0__tab');
+for (let i = 0; i < tabDoctors.length - 1; i++) {
+    tabDoctors[i].addEventListener('click', () => {
+        document.getElementById('doctor_dhh_2_0_0__content').innerHTML = infoTemp(doctorInfo[i])
+    })
+}
