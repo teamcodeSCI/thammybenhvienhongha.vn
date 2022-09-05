dbcf66ea19bd2d5776951cc9198085deba55cb32
doanduc2000 <doanminhducvip2000@gmail.com>
2022-09-05 11:15:14 +0700

    update

--- Header/header_dhh_2_0_0/js/header_dhh_2_0_0.js
+++ Header/header_dhh_2_0_0/js/header_dhh_2_0_0.js
@@ -1,15 +1,15 @@
 document.getElementById("headerMenuBtn").addEventListener("click", () => {
-  document.getElementById("headerSideBar").style.display = "block";
-  document.getElementById("headerBg").style.display = "block";
+    document.getElementById("headerSideBar").style.display = "block";
+    document.getElementById("headerBg").style.display = "block";
 });
 document.getElementById("headerBg").addEventListener("click", () => {
-  document.getElementById("headerSideBar").style.display = "none";
-  document.getElementById("headerBg").style.display = "none";
+    document.getElementById("headerSideBar").style.display = "none";
+    document.getElementById("headerBg").style.display = "none";
 });
 
-const menuItem = document.querySelectorAll(".header_dhh_2_0_0__menuItem");
+const menuItem = document.querySelectorAll(".header_dhh_2_0_0__item");
 menuItem.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("open");
-  });
+    item.addEventListener("click", () => {
+        item.classList.toggle("open");
+    });
 });
